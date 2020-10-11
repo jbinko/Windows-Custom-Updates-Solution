91c8b69eb4b23ac687f72d2eb3b0299b5a47b29e
jbinko <jbinko+github@gmail.com>
2020-10-11 09:27:50 +0200

    Version Updated

--- WVDCUS.Core/Version.cs
+++ WVDCUS.Core/Version.cs
@@ -4,6 +4,9 @@ namespace WVDCUS.Core
 {
     public sealed class Version
     {
-        public const string VersionNumber = "1.0.0.0";
+        public const string AssemblyVersion = "1.0.0.1";
+        public const string AssemblyFileVersion = AssemblyVersion;
+        public const string AssemblyProduct = "Windows Custom Updates Solution";
+        public const string AssemblyCopyright = "Copyright © 2020 Jiri Binko";
     }
 }
